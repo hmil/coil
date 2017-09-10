36a7fa78c735f7637481afdc0be095b085fe08c2
Hadrien Milano <hadrien.milano@gmail.com>
2017-09-10 02:14:12 +0200

    move files around

--- scripts/clean.sh.js
+++ scripts/clean.sh.js
@@ -1,7 +1,8 @@
 const { rm, set } = require('shelljs');
-const { DIST_DIR } = require('./constants');
+const { DIST_DIR, SRC_DIR } = require('./constants');
 
 // Abort script on error
 set('-e');
 
 rm('-rf', DIST_DIR);
+rm('-rf', `${SRC_DIR}/**/*.less.d.ts`);
